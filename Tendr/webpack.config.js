b3efd7e8a89d566fc1505ebf14c0f49a05d1e75f
Sergey Fadeev <fadeev@gmail.com>
2018-11-03 17:32:20 +0300

    fixed .gitignore

--- Tendr/webpack.config.js
+++ Tendr/webpack.config.js
@@ -20,7 +20,7 @@ module.exports = {
             { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
 
             // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-            { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
+            // { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
 
             { test: /\.css$/, use: [ "style-loader", "css-loader" ] }
         ]
