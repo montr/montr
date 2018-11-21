4e96d12ce92a0effa284d2bd5f37220662cb5019
Sergey Fadeev <fadeev@gmail.com>
2018-11-21 22:58:48 +0300

    added identity server project from template

---
+++ Idx/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Idx.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
