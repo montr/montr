d1b13321ae0028fe90cdb4e478ef3e0a779d7f66
Sergey Fadeev <fadeev@gmail.com>
2018-11-29 23:26:52 +0300

    changed event grid columns

--- Tendr/Services/IMetadataProvider.cs
+++ Tendr/Services/IMetadataProvider.cs
@@ -18,12 +18,12 @@ namespace Tendr.Services
 			{
 				result.Columns = new List<DataColumn>
 				{
-					new DataColumn { Key = "id", Name = "Номер", Sortable = true, Width = 50,
+					new DataColumn { Key = "id", Name = "Номер", Sortable = true, Width = 20,
 						UrlProperty = "url", DefaultSortOrder = SortOrder.Descending },
 					new DataColumn { Key = "configCode", Name = "Тип", Width = 60 },
-					new DataColumn { Key = "statusCode", Name = "Статус", Width = 60, Align = DataColumnAlign.Center },
-					new DataColumn { Key = "name", Name = "Наименование", Sortable = true, Width = 200, UrlProperty = "url" },
-					new DataColumn { Key = "description", Name = "Описание", Width = 300 },
+					new DataColumn { Key = "statusCode", Name = "Статус", Width = 60 /*, Align = DataColumnAlign.Center */ },
+					new DataColumn { Key = "name", Name = "Наименование", Sortable = true, Width = 400, UrlProperty = "url" },
+					// new DataColumn { Key = "description", Name = "Описание", Width = 300 },
 				};
 			}
 
