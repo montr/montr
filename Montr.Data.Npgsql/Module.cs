afe3d4c8ab91cf1991192a4aa4a7719d18abc27c
Sergey Fadeev <fadeev@gmail.com>
2019-01-27 20:56:03 +0300

    added npgsql logging (turned off), when added <PackageReference Include="Npgsql" Version="4.0.4" /> to project, TransactionScope'ed transactions broken

---
+++ Montr.Data.Npgsql/Module.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Montr.Modularity;
+using Npgsql.Logging;
+
+namespace Montr.Data.Npgsql
+{
+	public class Module: IModule
+	{
+		public void ConfigureServices(IServiceCollection services)
+		{
+			NpgsqlLogManager.Provider = new ConsoleLoggingProvider(NpgsqlLogLevel.Debug, true, true);
+		}
+	}
+}
