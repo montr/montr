8f2a68a3dfacc7a014dfa9ee44be4abc4a612fef
Sergey Fadeev <6033521+sfadeev@users.noreply.github.com>
2019-07-03 02:01:21 +0300

    restored classifier_tree.sql script

--- sql/Montr.MasterData/20190209-01-create-tables-for-classifiers.sql
+++ sql/Montr.MasterData/20190209-01-create-tables-for-classifiers.sql
@@ -1,15 +1,39 @@
+-- DROP TABLE public.classifier_tree CASCADE;
+-- SELECT * FROM information_schema.tables WHERE table_name = 'classifier_tree'
+
+CREATE TABLE IF NOT EXISTS public.classifier_tree
+(
+	uid uuid NOT NULL, -- DEFAULT uuid_generate_v1(),
+	type_uid uuid NOT NULL,
+    code character varying(32) NOT NULL COLLATE pg_catalog."default",
+    name character varying(2048) COLLATE pg_catalog."default",
+	CONSTRAINT classifier_tree_pk PRIMARY KEY (uid),
+	UNIQUE (type_uid, code)
+)
+WITH (
+	OIDS = FALSE
+);
+
+GRANT INSERT, SELECT, UPDATE, DELETE ON TABLE public.classifier_tree TO web;
+
+ALTER TABLE public.classifier_tree
+    ADD CONSTRAINT fk_classifier_tree_type_code FOREIGN KEY (type_uid)
+    REFERENCES public.classifier_type (uid) MATCH SIMPLE
+    ON UPDATE NO ACTION
+    ON DELETE NO ACTION;
+
 -- DROP TABLE public.classifier_group CASCADE;
 -- SELECT * FROM information_schema.tables WHERE table_name = 'classifier_group'
 
 CREATE TABLE IF NOT EXISTS public.classifier_group
 (
-	uid uuid NOT NULL, -- DEFAULT uuid_generate_v1(), 
-	type_uid uuid NOT NULL,
+	uid uuid NOT NULL, -- DEFAULT uuid_generate_v1(),
+	tree_uid uuid NOT NULL,
 	parent_uid uuid NULL,
-    code character varying(32) NOT NULL COLLATE pg_catalog."default", 
-    name character varying(2048) COLLATE pg_catalog."default", 
+    code character varying(32) NOT NULL COLLATE pg_catalog."default",
+    name character varying(2048) COLLATE pg_catalog."default",
 	CONSTRAINT classifier_group_pk PRIMARY KEY (uid),
-	UNIQUE (type_uid, code)
+	UNIQUE (tree_uid, code)
 )
 WITH (
 	OIDS = FALSE
@@ -18,8 +42,8 @@ WITH (
 GRANT INSERT, SELECT, UPDATE, DELETE ON TABLE public.classifier_group TO web;
 
 ALTER TABLE public.classifier_group
-    ADD CONSTRAINT fk_classifier_group_type_uid FOREIGN KEY (type_uid)
-    REFERENCES public.classifier_type (uid) MATCH SIMPLE
+    ADD CONSTRAINT fk_classifier_group_tree_uid FOREIGN KEY (tree_uid)
+    REFERENCES public.classifier_tree (uid) MATCH SIMPLE
     ON UPDATE NO ACTION
     ON DELETE NO ACTION;
 
@@ -34,12 +58,12 @@ ALTER TABLE public.classifier_group
 
 CREATE TABLE IF NOT EXISTS public.classifier
 (
-	uid uuid NOT NULL, -- DEFAULT uuid_generate_v1(), 
+	uid uuid NOT NULL, -- DEFAULT uuid_generate_v1(),
 	type_uid uuid NOT NULL,
-    status_code character varying(16) NOT NULL COLLATE pg_catalog."default", 
+    status_code character varying(16) NOT NULL COLLATE pg_catalog."default",
 	parent_uid uuid NULL,
-    code character varying(32) NOT NULL COLLATE pg_catalog."default", 
-    name character varying(2048) COLLATE pg_catalog."default", 
+    code character varying(32) NOT NULL COLLATE pg_catalog."default",
+    name character varying(2048) COLLATE pg_catalog."default",
 	CONSTRAINT classifier_pk PRIMARY KEY (uid),
 	UNIQUE (type_uid, code)
 )
@@ -66,7 +90,7 @@ ALTER TABLE public.classifier
 
 CREATE TABLE IF NOT EXISTS public.classifier_link
 (
-	group_uid uuid NOT NULL, 
+	group_uid uuid NOT NULL,
 	item_uid uuid NOT NULL,
 	CONSTRAINT classifier_link_pk PRIMARY KEY (group_uid, item_uid)
 )
@@ -81,19 +105,19 @@ ALTER TABLE public.classifier_link
     REFERENCES public.classifier_group (uid) MATCH SIMPLE
     ON UPDATE NO ACTION
     ON DELETE NO ACTION;
-	
+
 ALTER TABLE public.classifier_link
     ADD CONSTRAINT fk_classifier_link_item_uid FOREIGN KEY (item_uid)
     REFERENCES public.classifier (uid) MATCH SIMPLE
     ON UPDATE NO ACTION
     ON DELETE NO ACTION;
-	
+
 -- DROP TABLE public.classifier_closure;
 -- SELECT * FROM information_schema.tables WHERE table_name = 'classifier_closure'
 
 CREATE TABLE IF NOT EXISTS public.classifier_closure
 (
-	parent_uid uuid NOT NULL, 
+	parent_uid uuid NOT NULL,
 	child_uid uuid NOT NULL,
 	level smallint NOT NULL,
 	CONSTRAINT classifier_closure_pk PRIMARY KEY (parent_uid, child_uid, level)
