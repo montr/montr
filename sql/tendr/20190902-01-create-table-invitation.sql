c9e57554909b2e5bae7dc99a250b4915abdf7287
Sergey Fadeev <6033521+sfadeev@users.noreply.github.com>
2019-09-06 00:06:21 +0300

    fixed invitation table script

--- sql/tendr/20190902-01-create-table-invitation.sql
+++ sql/tendr/20190902-01-create-table-invitation.sql
@@ -4,8 +4,10 @@
 
 CREATE TABLE public.invitation
 (
-    uid uuid NOT NULL DEFAULT uuid_generate_v1(),
-    company_uid uuid NOT NULL,
+    uid uuid NOT NULL, -- DEFAULT uuid_generate_v1(),
+    event_uid uuid NOT NULL,
+	status_code character varying(16) COLLATE pg_catalog."default",
+    counterparty_uid uuid NOT NULL,
     CONSTRAINT invitation_pk PRIMARY KEY (uid)
 )
 WITH (
@@ -16,7 +18,13 @@ TABLESPACE pg_default;
 GRANT INSERT, SELECT, UPDATE, DELETE ON TABLE public.invitation TO web;
 
 ALTER TABLE public.invitation
-    ADD CONSTRAINT fk_invitation_coompany_uid FOREIGN KEY (company_uid)
-    REFERENCES public.company (uid) MATCH SIMPLE
+    ADD CONSTRAINT fk_invitation_event_uid FOREIGN KEY (event_uid)
+    REFERENCES public.event (uid) MATCH SIMPLE
+    ON UPDATE NO ACTION
+    ON DELETE NO ACTION;
+
+ALTER TABLE public.invitation
+    ADD CONSTRAINT fk_invitation_counterparty_uid FOREIGN KEY (counterparty_uid)
+    REFERENCES public.classifier (uid) MATCH SIMPLE
     ON UPDATE NO ACTION
     ON DELETE NO ACTION;
