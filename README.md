f5432c867fd74dc668095d948ae266d5aab1d9a3
Sergey Fadeev <6033521+sfadeev@users.noreply.github.com>
2019-09-22 15:20:16 +0300

    changed badges

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 <p>
-	<a href="https://github.com/montr/montr/actions">
+	<a href="https://github.com/montr/montr">
 		<img alt="GitHub Actions status" src="https://github.com/montr/montr/workflows/build/badge.svg">
 	</a>
 </p>
--- src/ui/modules/tendr/components/footer.tsx
+++ src/ui/modules/tendr/components/footer.tsx
@@ -1,12 +1,11 @@
 import * as React from "react";
-import { Icon } from "antd";
 
 export const Footer = () => {
 	return (<>
 		© {new Date().getFullYear()} Montr
 
-		<a href="https://github.com/montr/montr" style={{ color: "gray", float: "right" }} target="_blank">
-			<Icon type="github" />
+		<a href="https://github.com/montr/montr/actions" style={{ float: "right" }} target="_blank">
+			<img alt="GitHub Actions status" src="https://github.com/montr/montr/workflows/build/badge.svg" />
 		</a>
 	</>);
 }
