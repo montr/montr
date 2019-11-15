9cb2cd613d0e979ceb27d97fbb880ae9a31033c0
Sergey Fadeev <6033521+sfadeev@users.noreply.github.com>
2019-11-15 08:07:23 +0300

    temp solution to change antd controls lang

--- src/ui/modules/host/app.tsx
+++ src/ui/modules/host/app.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { ConfigProvider } from "antd";
-import ru_RU from "antd/lib/locale-provider/ru_RU";
+// import locale from "antd/lib/locale-provider/ru_RU";
 import { Layout } from "@montr-core/constants";
 import { AppLayoutRegistry, AppRouteRegistry } from "@montr-core/services";
 import { ErrorBoundary, UserContextProvider, AuthCallbackHandler, AppRouteList, SuspenseFallback } from "@montr-core/components";
@@ -11,6 +11,7 @@ import { PublicLayout, PrivateLayout } from "./components";
 import "@montr-core/i18n";
 import "@montr-core/index.less"
 import "./modules"
+import { Translation } from "react-i18next";
 
 AppLayoutRegistry.register(Layout.public, PublicLayout);
 AppLayoutRegistry.register(Layout.private, PrivateLayout);
@@ -20,20 +21,22 @@ class App extends React.Component {
 		return (
 			<React.Suspense fallback={<SuspenseFallback />}>
 				<ErrorBoundary>
-					<ConfigProvider locale={ru_RU}>
-						<UserContextProvider>
-							<CompanyContextProvider>
-								<AuthCallbackHandler>
-									<AppRouteList
-										routes={AppRouteRegistry.Routes}
-										layoutRegistry={AppLayoutRegistry.get}
-										defaultLayout={Layout.private}
-										errorLayout={Layout.public}
-									/>
-								</AuthCallbackHandler>
-							</CompanyContextProvider>
-						</UserContextProvider>
-					</ConfigProvider>
+					<Translation>{(t, { i18n, lng }) =>
+						<ConfigProvider locale={{ locale: lng }}>
+							<UserContextProvider>
+								<CompanyContextProvider>
+									<AuthCallbackHandler>
+										<AppRouteList
+											routes={AppRouteRegistry.Routes}
+											layoutRegistry={AppLayoutRegistry.get}
+											defaultLayout={Layout.private}
+											errorLayout={Layout.public}
+										/>
+									</AuthCallbackHandler>
+								</CompanyContextProvider>
+							</UserContextProvider>
+						</ConfigProvider>}
+					</Translation>
 				</ErrorBoundary>
 			</React.Suspense>
 		);
