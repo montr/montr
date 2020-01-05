908d19c9676c9ff1f4769ac76f38b37381be4a05
Sergey Fadeev <6033521+sfadeev@users.noreply.github.com>
2020-01-06 02:12:10 +0300

    further fixes after antd migration: - fixed data table display complex data (keys with dots) - data not displayed initially on classifier and classifier type edit pages

--- src/ui/modules/montr-core/components/data-table.tsx
+++ src/ui/modules/montr-core/components/data-table.tsx
@@ -148,7 +148,7 @@ export class DataTable<TModel extends IIndexer> extends React.Component<IProps<T
 
 			return {
 				key: item.key,
-				dataIndex: item.path || item.key,
+				dataIndex: (item.path || item.key).split("."),
 				title: item.name,
 				align: item.align,
 				sorter: item.sortable,
