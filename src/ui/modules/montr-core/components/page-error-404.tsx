16993f39c15b473dc0f83262487037d155fb03db
Sergey Fadeev <6033521+sfadeev@users.noreply.github.com>
2020-02-24 21:16:23 +0300

    updated to antd 4.0.0-rc.6

--- src/ui/modules/montr-core/components/page-error-404.tsx
+++ src/ui/modules/montr-core/components/page-error-404.tsx
@@ -5,8 +5,8 @@ export default class Error404 extends React.Component {
 	render() {
 		return (
 			<Result
-				status="404"
-				title="404"
+				// status={404}
+				title={<h2>404</h2>}
 				subTitle="Sorry, the page you visited does not exist."
 				extra={<Button type="primary">Back Home</Button>}
 			/>
