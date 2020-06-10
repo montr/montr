1c0b52598e3320a10e18e41fda3b33ec3ca35127
Sergey Fadeev <6033521+sfadeev@users.noreply.github.com>
2020-06-10 23:34:10 +0300

    updated client libs, small fix for notify by email action

--- src/ui/modules/montr-core/components/notify-by-email-automation-action.tsx
+++ src/ui/modules/montr-core/components/notify-by-email-automation-action.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Space, Input, Form } from "antd";
 import { INotifyByEmailAutomationAction } from "../models";
 import { IAutomationActionProps } from "./automation-field-factory";
+import TextArea from "antd/lib/input/TextArea";
 
 interface IProps extends IAutomationActionProps {
 	action: INotifyByEmailAutomationAction;
@@ -23,6 +24,24 @@ export class NotifyByEmailAutomationAction extends React.Component<IProps> {
 					fieldKey={[item.fieldKey, "recipient"]}
 					rules={[{ required: true }]}>
 					<Input placeholder="Recipient" />
+				</Form.Item>
+
+				<Form.Item
+					{...item}
+					label="Subject"
+					name={[item.name, "subject"]}
+					fieldKey={[item.fieldKey, "subject"]}
+					rules={[{ required: true }]}>
+					<Input placeholder="Subject" />
+				</Form.Item>
+
+				<Form.Item
+					{...item}
+					label="Body"
+					name={[item.name, "body"]}
+					fieldKey={[item.fieldKey, "body"]}
+					rules={[{ required: true }]}>
+					<TextArea placeholder="Body" />
 				</Form.Item>
 
 			</Space>
