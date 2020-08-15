f0c6a79a980930518c2a6a18cb6584520b884669
Sergey Fadeev <6033521+sfadeev@users.noreply.github.com>
2020-08-16 00:00:08 +0300

    updated dotnet to 3.1.7

---
+++ src/Montr.Core/Controllers/EntityStatusController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Montr.Core.Controllers
+{
+	[ApiController, Route("api/[controller]/[action]")]
+	public class EntityStatusController : ControllerBase
+	{
+		[HttpPost]
+		public async Task<SearchResult<Automation>> List(GetAutomationList request)
+		{
+			return await _mediator.Send(request);
+		}
+
+		[HttpPost]
+		public async Task<Automation> Get(GetAutomation request)
+		{
+			return await _mediator.Send(request);
+		}
+
+		[HttpPost]
+		public async Task<ApiResult> Insert(InsertAutomation request)
+		{
+			return await _mediator.Send(request);
+		}
+
+		[HttpPost]
+		public async Task<ApiResult> Update(UpdateAutomation request)
+		{
+			return await _mediator.Send(request);
+		}
+
+		[HttpPost]
+		public async Task<ApiResult> Delete(DeleteAutomation request)
+		{
+			return await _mediator.Send(request);
+		}}
+}
