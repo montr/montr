261d8c9befa84dff47eba317f528bdfac64932d9
Sergey Fadeev <6033521+sfadeev@users.noreply.github.com>
2020-10-20 22:08:18 +0300

    removed unused ts-loader, updated hangfire

--- src/ui/webpack.common.js
+++ src/ui/webpack.common.js
@@ -31,7 +31,6 @@ const options = {
 			{
 				test: /\.(jsx|tsx|js|ts)$/,
 				loader: "babel-loader",
-				/* loader: "ts-loader", */
 				exclude: /node_modules/
 			},
 
@@ -43,9 +42,13 @@ const options = {
 			},
 
 			{
-				test: /\.less$/, use: [
-					"style-loader",
-					"css-loader",
+				test: /\.less$/,
+				use: [
+					{
+						loader: 'style-loader',
+					}, {
+						loader: 'css-loader', // translates CSS into CommonJS
+					},
 					{
 						loader: "less-loader",
 						options: {
