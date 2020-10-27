736422efefdcdedcc38a6d67b2d94958d0b24643
Sergey Fadeev <6033521+sfadeev@users.noreply.github.com>
2020-10-27 22:14:21 +0300

    updated client libs

--- src/ui/webpack.common.js
+++ src/ui/webpack.common.js
@@ -33,14 +33,12 @@ const options = {
 				loader: "babel-loader",
 				exclude: /node_modules/
 			},
-
-			// All output ".js" files will have any sourcemaps re-processed by "source-map-loader".
 			{
 				test: /\.js$/,
 				loader: "source-map-loader",
-				enforce: "pre", exclude: [/mutationobserver-shim/g]
+				enforce: "pre",
+				exclude: [/mutationobserver-shim/g]
 			},
-
 			{
 				test: /\.less$/,
 				use: [
