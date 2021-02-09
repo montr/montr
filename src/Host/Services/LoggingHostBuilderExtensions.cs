75669e6afa77ab822feed0a923c2bc9e3f198aff
Sergey Fadeev <fadeev@gmail.com>
2021-02-09 23:45:48 +0300

    updated sentry, configured sentry log level

--- src/Host/Services/LoggingHostBuilderExtensions.cs
+++ src/Host/Services/LoggingHostBuilderExtensions.cs
@@ -19,12 +19,13 @@ namespace Host.Services
 
 		private static void ConfigureLogger(IHostEnvironment environment, LoggerConfiguration configuration)
 		{
-			configuration.MinimumLevel.Debug()
+			configuration/*.MinimumLevel.Debug()
 				.MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
 				.MinimumLevel.Override("System", LogEventLevel.Warning)
 				.MinimumLevel.Override("Microsoft.AspNetCore.Authentication", LogEventLevel.Information)
 				.MinimumLevel.Override("Hangfire.Processing.BackgroundExecution", LogEventLevel.Information)
 				.MinimumLevel.Override("Hangfire.Server.ServerHeartbeatProcess", LogEventLevel.Information)
+				.MinimumLevel.Override("Sentry.ISentryClient", LogEventLevel.Information)*/
 				.Enrich.FromLogContext()
 				.WriteTo.File($"../../../.logs/{typeof(Startup).Namespace}-{environment.EnvironmentName}.log")
 				.WriteTo.Console(outputTemplate: "{Timestamp:o} [{Level:w4}] {SourceContext} - {Message:lj}{NewLine}{Exception}");
