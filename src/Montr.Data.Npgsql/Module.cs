8a4a4ded4067fae34c474b72c3cac6fbce574710
Sergey Fadeev <fadeev@gmail.com>
2021-11-29 00:47:14 +0300

    temp fix for hangfire error added recent hangfire migrations scripts

--- src/Montr.Data.Npgsql/Module.cs
+++ src/Montr.Data.Npgsql/Module.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using System;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Montr.Core;
 using Npgsql.Logging;
@@ -12,6 +13,11 @@ namespace Montr.Data.Npgsql
 		{
 			NpgsqlLogManager.IsParameterLoggingEnabled = true;
 			// NpgsqlLogManager.Provider = new ConsoleLoggingProvider(NpgsqlLogLevel.Debug, true, true);
+
+			// todo: remove
+			// https://github.com/frankhommers/Hangfire.PostgreSql/issues/214
+			// https://www.npgsql.org/doc/release-notes/6.0.html#parameter-names-now-use-case-sensitive-matching
+			AppContext.SetSwitch("Npgsql.EnableLegacyCaseInsensitiveDbParameters", true);
 		}
 	}
 }
