266bdaadcebd8743e7e6dbf64ffe178641fecbb7
Sergey Fadeev <fadeev@gmail.com>
2021-12-07 23:40:53 +0300

    automation: restored transactions in automations runner

--- src/Montr.Automate.Impl/CommandHandlers/RunAutomationsHandler.cs
+++ src/Montr.Automate.Impl/CommandHandlers/RunAutomationsHandler.cs
@@ -5,23 +5,24 @@ using Montr.Automate.Commands;
 using Montr.Automate.Models;
 using Montr.Automate.Services;
 using Montr.Core.Models;
+using Montr.Core.Services;
 
 namespace Montr.Automate.Impl.CommandHandlers
 {
 	public class RunAutomationsHandler : IRequestHandler<RunAutomations, ApiResult>
 	{
-		// private readonly IUnitOfWorkFactory _unitOfWorkFactory;
+		private readonly IUnitOfWorkFactory _unitOfWorkFactory;
 		private readonly IAutomationRunner _automationRunner;
 
-		public RunAutomationsHandler(/*, IUnitOfWorkFactory unitOfWorkFactory,*/ IAutomationRunner automationRunner)
+		public RunAutomationsHandler(IUnitOfWorkFactory unitOfWorkFactory, IAutomationRunner automationRunner)
 		{
-			// _unitOfWorkFactory = unitOfWorkFactory;
+			_unitOfWorkFactory = unitOfWorkFactory;
 			_automationRunner = automationRunner;
 		}
 
 		public async Task<ApiResult> Handle(RunAutomations request, CancellationToken cancellationToken)
 		{
-			// using (var scope = _unitOfWorkFactory.Create())
+			using (var scope = _unitOfWorkFactory.Create())
 			{
 				var result = await _automationRunner.Run(new AutomationContext
 				{
@@ -30,7 +31,7 @@ namespace Montr.Automate.Impl.CommandHandlers
 					EntityUid = request.EntityUid
 				}, cancellationToken);
 
-				// scope.Commit();
+				if (result.Success) scope.Commit();
 
 				return result;
 			}
