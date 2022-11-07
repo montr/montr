01145e17de2451afe3e6440e022c71008c5be416
Sergey Fadeev <fadeev@gmail.com>
2022-11-07 20:56:57 +0300

    core ui: fixed obsolete usage of antd dropdown overlay

--- src/ui/modules/montr-core/components/language-selector.tsx
+++ src/ui/modules/montr-core/components/language-selector.tsx
@@ -1,9 +1,8 @@
 import { Dropdown } from "antd";
-import { i18n } from "i18next";
 import * as React from "react";
 import { Translation } from "react-i18next";
-import { DataMenu, Icon } from ".";
-import { IIndexer, IMenu } from "../models";
+import { Icon } from ".";
+import { IIndexer } from "../models/indexer";
 
 export const LanguageSelector = () => {
 
@@ -13,19 +12,17 @@ export const LanguageSelector = () => {
 		"ru": { title: "русский" },
 	};
 
-	function getOverlayItems(i18n: i18n): IMenu[] {
-		return Object.keys(langs).map(lng => ({
-			name: langs[lng].title,
-			onClick: () => i18n.changeLanguage(lng)
-		}));
-	}
-
 	return (
 		<Translation>
 			{(t, { i18n }) => (
 				<Dropdown
 					trigger={["click"]}
-					overlay={<DataMenu tail={getOverlayItems(i18n)} />}>
+					menu={{
+						items: Object.keys(langs).map(lng => ({
+							key: lng, label: langs[lng].title
+						})),
+						onClick: (item) => i18n.changeLanguage(item.key)
+					}}>
 					<a className="ant-dropdown-link">
 						{Icon.Global} {langs[i18n.language].title} {Icon.Down}
 					</a>
