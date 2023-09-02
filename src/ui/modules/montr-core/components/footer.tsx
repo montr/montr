e4d81be4c8de85a15ddf1126ddccfaaed39b83c7
Sergey Fadeev <fadeev@gmail.com>
2023-09-02 13:26:14 +0300

    updated client libs and packages

--- src/ui/modules/montr-core/components/footer.tsx
+++ src/ui/modules/montr-core/components/footer.tsx
@@ -5,9 +5,9 @@ export const Footer = (): React.ReactElement => {
 	return <>
 		<span>Powered by <a href="https://github.com/montr/montr" target="_blank">Montr</a></span>
 
-		<a href="https://github.com/montr/montr/actions" style={{ float: "right" }} target="_blank">
+		{/* <a href="https://github.com/montr/montr/actions" style={{ float: "right" }} target="_blank">
 			<img alt="GitHub Actions status" src="//github.com/montr/montr/workflows/build/badge.svg" />
-		</a>
+		</a> */}
 
 		<LanguageSelector />
 	</>;
